0e1ad1655a0d9a9dd7a5d351a3de4500cb0149ff
claurennt <c.benedetti1989@gmail.com>
2021-10-29 14:49:48 +0200

    1st step,display uplaoded pic

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const multer = require("multer");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./public/images");
+  },
+  filename: (req, file, cb) => {
+    const extension = file.mimetype.split("/")[1];
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, `${file.fieldname}-${uniqueSuffix}.${extension}`);
+  },
 });
 
+const upload = multer({ storage: storage });
+
+/* GET home page. */
+router.get("/", (req, res, next) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+router.post("/upload-profile-pic", upload.single("profile_pic"), (req, res) => {
+  if (!req.file) {
+    return res.status(404).send("No file received");
+  }
+
+  res
+    .status(200)
+    .send(
+      `<h2>Here is the picture:</h2><br/><img src="/images/${req.file.filename}" alt="profile pic"/>`
+    );
+});
 module.exports = router;
